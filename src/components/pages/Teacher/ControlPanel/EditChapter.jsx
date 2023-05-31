92443451c43521af90bf5d45913fec2828a52e18
twoballs-ai <twoballs20@gmail.com>
2023-05-31 13:55:03 +0300

    добавление видео

--- src/components/pages/Teacher/ControlPanel/EditChapter.jsx
+++ src/components/pages/Teacher/ControlPanel/EditChapter.jsx
@@ -15,21 +15,29 @@ const baseUrl = 'http://127.0.0.1:8000/api/'
 function EditChapter() {
     const {chapter_id} = useParams()
     const [chapterEditData, setChapterEditData] = useState({
-      chapter: chapter_id,
+      course: '',
       title:'',
       description:'',
+      prev_video:'',
       video:'',
       comment:''
     })
 console.log(chapterEditData)
     useEffect(()=>{
         axios
-        .get(baseUrl+'course-chapter/'+chapter_id
+        .get(baseUrl+'chapter/'+chapter_id
           // ,{ headers: { Authorization: `Token da0d550bcc813a1b1cc6b905551cb11e3bf95046` } }
           // ,{headers: { "Content-Type": "multipart/form-data" }}
           )
         .then(response => {
-            setChapterEditData(response.data)
+            setChapterEditData({
+              course: response.data.course,
+              title: response.data.title,
+              description: response.data.description,
+              prev_video: response.data.video,
+              video:'',
+              comment: response.data.comment
+            })
             // setTotalResult(response.data.length)
           console.log(response.data)
         })
@@ -52,14 +60,34 @@ console.log(chapterEditData)
 
       const formSubmit=(e)=>{
         e.preventDefault()
-    
+     const _formData = new FormData()
+     _formData.append('course',chapterEditData.course)
+     _formData.append('title',chapterEditData.title)
+     _formData.append('description',chapterEditData.description)
+     if(chapterEditData.video !==''){
+      _formData.append('video',chapterEditData.video,chapterEditData.video.name)
+     }
+     _formData.append('comment',chapterEditData.comment)
+     console.log(chapterEditData)
+
         axios
-        .post(baseUrl+'chapter/', chapterEditData
+        .put(baseUrl+'chapter/'+chapter_id, _formData
           // ,{ headers: { Authorization: `Token da0d550bcc813a1b1cc6b905551cb11e3bf95046` } }
           ,{headers: { "Content-Type": "multipart/form-data" }}
           )
         .then(response => {
-          console.log(response)
+          if(response.status==200){
+            Swal.fire({
+              position: 'top-end',
+              icon: 'success',
+              title: 'Ваши данные обновлены',
+              toast:true,
+              timerProgressBar:true,
+              showConfirmButton: false,
+              timer: 3000
+            })
+          }
+        
      
           // window.location.href='/teacher-profile/my-courses'
     
@@ -74,7 +102,7 @@ console.log(chapterEditData)
 
       <Form.Group className="mb-3" controlId="formBasicCategory">
         <Form.Label>Название</Form.Label>
-        <Form.Control name="title" type="text" placeholder="категория" onChange={handleChange} />
+        <Form.Control name="title" value={chapterEditData.title} type="text" placeholder="категория" onChange={handleChange} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicCategory">
@@ -83,11 +111,18 @@ console.log(chapterEditData)
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCategory">
         <Form.Label>Видео урок</Form.Label>
-        <Form.Control name="video"   type="file" placeholder="Видео урок" onChange={handleFileChange} />
+        <Form.Control name="video"  type="file" placeholder="Видео урок" onChange={handleFileChange} />
       </Form.Group>
+      {chapterEditData.prev_video && 
+      <video width="100%"  controls>
+        <source src={chapterEditData.prev_video} type="video/mp4" />
+  {/* <source src={chapterEditData.prev_video} type="video/ogg" /> */}
+  Your browser does not support the video tag.
+</video >
+}
       <Form.Group className="mb-3" controlId="formBasicCategory">
         <Form.Label>Комментарии автора</Form.Label>
-        <Form.Control name="comment" as="textarea" rows={3} placeholder="Комментарии автора" onChange={handleChange} />
+        <Form.Control name="comment" value={chapterEditData.comment} as="textarea" rows={3} placeholder="Комментарии автора" onChange={handleChange} />
       </Form.Group>
     
       <Button onClick={formSubmit} variant="primary" type="submit">
