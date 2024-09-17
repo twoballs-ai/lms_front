ece42d71709a1323b8cb4ed034beac5a2a02423c
twoballs-ai <twoballs@gmail.com>
2024-09-17 21:49:51 +0300

    fix

--- src/components/reUseComponents/Cards.tsx
+++ src/components/reUseComponents/Cards.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Image from 'next/image'; // Import the Image component
 import { Card } from 'antd';
 import './CustomCard.scss';
+import ImageViewer from './ImageViewer';
 
 const { Meta } = Card;
 
@@ -10,14 +10,14 @@ const CustomCard = ({ title, description, image }) => (
     hoverable
     className="custom-card"
     cover={
-      <Image
-        alt={title}
-        src={image}
-        layout="responsive"
-        width={400} // Set appropriate width
-        height={250} // Set appropriate height
-        className="card-image"
-      />
+
+      <ImageViewer
+      alt={title}
+      src={image}
+      layout="responsive"
+      width={400} // Set appropriate width
+      height={250} // Set appropriate height
+  />
     }
   >
     <Meta title={title} description={description} className="card-meta" />
