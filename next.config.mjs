79aa63c5b97999963dd659fd64b16f63589a5549
twoballs-ai <twoballs20@gmail.com>
2024-09-17 21:08:11 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['127.0.0.1'], // Add your allowed hostnames here
+    domains: ['backend.courserio.ru','127.0.0.1'], // Add your allowed hostnames here
   },
   typescript: {
     ignoreBuildErrors: true, // Отключает проверку ошибок TypeScript при сборке
